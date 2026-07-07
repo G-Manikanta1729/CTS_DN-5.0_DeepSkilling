aba5a9196012fb59187f59d330b124338078f84b
G-Manikanta1729 <grandhemanikanta65@gmail.com>
2026-07-07 05:59:15 +0530

    Added Week 2 Spring Data JPA Handson Exercise 4

---
+++ Week_2_Spring_Framework/spring-data-jpa-handson/Exercise_4_Add_New_Country/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,56 @@
+package com.cognizant.orm_learn;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.service.CountryService;
+import com.cognizant.orm_learn.service.exception.CountryNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+	private static final Logger LOGGER =
+			LoggerFactory.getLogger(OrmLearnApplication.class);
+
+	private static CountryService countryService;
+
+	public static void main(String[] args) throws CountryNotFoundException {
+
+		ApplicationContext context =
+				SpringApplication.run(
+						OrmLearnApplication.class,
+						args);
+
+		countryService =
+				context.getBean(CountryService.class);
+
+		testAddCountry();
+
+	}
+
+	private static void testAddCountry()
+			throws CountryNotFoundException {
+
+		LOGGER.info("Start");
+
+		Country country = new Country();
+
+		country.setCode("NP");
+
+		country.setName("Nepal");
+
+		countryService.addCountry(country);
+
+		Country addedCountry =
+				countryService.findCountryByCode("NP");
+
+		LOGGER.debug("Country : {}", addedCountry);
+
+		LOGGER.info("End");
+
+	}
+
+}
---
+++ Week_2_Spring_Framework/spring-data-jpa-handson/Exercise_4_Add_New_Country/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
@@ -0,0 +1,47 @@
+package com.cognizant.orm_learn.service;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+import com.cognizant.orm_learn.service.exception.CountryNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional(readOnly = true)
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Country findCountryByCode(String countryCode)
+            throws CountryNotFoundException {
+
+        Optional<Country> result =
+                countryRepository.findById(countryCode);
+
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException(
+                    "Country not found: " + countryCode);
+        }
+
+        return result.get();
+    }
+
+
+    @Transactional
+    public void addCountry(Country country) {
+
+        countryRepository.save(country);
+
+    }
+
+}
