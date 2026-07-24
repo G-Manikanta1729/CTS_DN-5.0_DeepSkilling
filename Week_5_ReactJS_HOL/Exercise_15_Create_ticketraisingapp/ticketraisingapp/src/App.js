4bb799d4b1358d0c1d5a850fa6ced163b4ae2989
G-Manikanta1729 <grandhemanikanta65@gmail.com>
2026-07-24 15:44:50 +0530

    Added Week 5 Exercise 15 Create ticket raising app

---
+++ Week_5_ReactJS_HOL/Exercise_15_Create_ticketraisingapp/ticketraisingapp/src/App.js
@@ -0,0 +1,14 @@
+import ComplaintRegister from "./components/ComplaintRegister";
+import "./App.css";
+
+function App() {
+
+  return (
+
+    <ComplaintRegister />
+
+  );
+
+}
+
+export default App;
